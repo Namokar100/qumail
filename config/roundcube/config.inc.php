d1f84cdb84092de2e765b7285d49e7d0e696e4bb
Namokar100 <namokarsvg@gmail.com>
2025-12-11 13:16:25 +0530

    add custom php to bypass checks in r-cube

---
+++ config/roundcube/config.inc.php
@@ -0,0 +1,30 @@
+<?php
+// Initialize config array as per default Roundcube behavior
+$config = [];
+$config['plugins'] = [];
+$config['log_driver'] = 'stdout';
+$config['zipdownload_selection'] = true;
+// Random key for security (normally generated, but static here is fine for dev)
+$config['des_key'] = '6WgUaGloMCvXRotjY/JOyJ6W'; 
+$config['enable_spellcheck'] = true;
+$config['spellcheck_engine'] = 'pspell';
+
+// Include Docker defaults
+include(__DIR__ . '/config.docker.inc.php');
+
+// Disable SSL verification for local self-signed certs
+$config['imap_conn_options'] = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ],
+];
+
+$config['smtp_conn_options'] = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ],
+];
